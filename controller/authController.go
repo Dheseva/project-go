07f01507c52acce4fceafee2c7088b2c279d9f22
catura <catura.ba@gmail.com>
2023-09-14 13:12:01 +0900

    jwt-cookies

--- controller/authController.go
+++ controller/authController.go
@@ -1,12 +1,20 @@
 package controller
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
 	"project-go/database"
 	"project-go/models"
+	"strconv"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 )
+
+//const SecretKey = "secret" previous privatekey
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
@@ -24,4 +32,78 @@ func Register(c *fiber.Ctx) error {
 
 	database.DB.Create(&user)
 	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil{
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0{
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"status": false,
+			"message": "user not found",
+			"data": nil,
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil{
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"status": false,
+			"message": "incorrect password",
+			"data": nil,
+		})
+	}
+
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+
+	if err != nil{
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"status": false,
+			"error": err.Error(),
+			"message": "could get private key",
+		})
+	}
+
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.StandardClaims{
+		Issuer: strconv.Itoa(int(user.Id)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), // 1day
+	})
+
+	token, err := claims.SignedString(privateKey)
+
+	if err != nil{
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"status": false,
+			"error": err.Error(),
+			"message": "could not log in",
+			"data": user,
+		})
+	}
+
+	cookie := fiber.Cookie{
+		Name: "jwt",
+		Value: token,
+		Expires: time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"status": true,
+		"message": "success log in",
+		"data": user,
+	})
 }
