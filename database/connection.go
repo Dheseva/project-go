2f189ba760455db32c188d277b4e0249687776bc
catura <catura.ba@gmail.com>
2023-09-17 20:59:32 +0900

    db

--- database/connection.go
+++ database/connection.go
@@ -12,7 +12,7 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	mysqlDb := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v", 
+	mysqlDb := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", 
 			os.Getenv("DB_USER"),
 			os.Getenv("DB_PASSWORD"),
 			os.Getenv("DB_HOST"),
